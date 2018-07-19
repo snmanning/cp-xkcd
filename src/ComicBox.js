11861b9641b836a6549997fd4c66ed064ca8654c
Sarah Manning <sarahnmanning@gmail.com>
2018-07-18 21:00:35 -0400

    added componentDidMount and made a web request for json date. Rendered based on possible outcomes . Need to address xkcd cors issue before moving forward

--- src/ComicBox.js
+++ src/ComicBox.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
 import './ComicBox.css';
+import axios from 'axios';
 
 class ComicBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+            comic: {},
+            success: false,
+            error: null,
+        };
+    }
+
+componentDidMount() {
+    // const url = 'https://xkcd.com/614/info.0.json';
+    const url = 'https://jsonplaceholder.typicode.com/users/1';
+    axios.get(url).then(response => {
+        this.setState({
+            comic: response.data,
+            success: true,
+        });
+    }).catch((error) => {
+            this.setState({
+                success: false,
+                error: error,
+            });
+    });
+}
+
     render () {
+        const { success, error } = this.state;
+        if(error) {
+            return (
+                <p>
+                    Stuff done broke...
+                </p>
+            );
+        }
+        if(!success) {
+            return (
+            <h1>
+                Loading...
+            </h1>
+            );
+        }
+
         return (
             <div className='ComicBox-container'>
                 Here be comics
